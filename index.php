b402dc2ce7344bb17af0c477feef3713b0d1347e
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-05-29 18:40:51 +0200

    corrigé

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+    // Exercice 4
+    $name = 'Erwan';
+    $cava = 'Comment vas-tu ?'; ?>
+    <?php echo 'Salam', ', ', $name, '. ', $cava; ?>
+
+  </body>
+</html>
